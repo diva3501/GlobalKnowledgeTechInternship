e4fab7502eb1de3a6d82ae9cf376cdba5f135332
Rithik Raja S <rithikraja28.rr@gmail.com>
2024-06-21 14:48:21 +0530

    update ui

--- frontend/src/components/Partnership/Partnership.jsx
+++ frontend/src/components/Partnership/Partnership.jsx
@@ -1,8 +1,10 @@
-import React, { useState } from 'react';
-import './Partnership.css';
-import { FaAngleDoubleDown } from 'react-icons/fa';
+import React, { useState } from "react";
+import "./Partnership.css";
+import { FaAngleDoubleDown } from "react-icons/fa";
+import Card from "react-bootstrap/Card";
 
 const partners = [
+<<<<<<< Updated upstream
   { name: 'Microsoft', image: '/partner1.png' },
   { name: 'Artificial Intelligence', image: '/partner2.png' },
   { name: 'Anglepoint', image: '/partner3.jpeg' },
@@ -11,6 +13,16 @@ const partners = [
   { name: 'Mulesoft', image: '/partner6.png' },
   { name: 'Blockchain', image: '/partner7.png' },
   { name: 'IBM', image: '/partner8.png' }
+=======
+  { name: "Microsoft", image: "/partner1.png" },
+  { name: "Artificial Intelligence", image: "/partner2.png" },
+  { name: "Anglepoint", image: "/partner3.jpeg" },
+  { name: "Red Hat", image: "/partner4.jpg" },
+  { name: "Tableau", image: "/partner5.jpg" },
+  { name: "Mulesoft", image: "/partner6.jpg" },
+  { name: "Blockchain", image: "/partner7.jpg" },
+  { name: "IBM", image: "/partner8.jpg" },
+>>>>>>> Stashed changes
 ];
 
 const Partnership = () => {
@@ -20,20 +32,32 @@ const Partnership = () => {
 
   return (
     <div className="partnership">
-      <h2 className="heading">Our Partnership</h2>
-      <div className="card-container">
+      <h2 className="heading">Our Partnerships</h2>
+      <div className="card-container d-flex justify-content-center mt-5 mb-5 gap-5 flex-wrap">
         {displayedPartners.map((partner, index) => (
-          <div className="card" key={index}>
-            <img src={partner.image} alt={partner.name} className="card-image" />
-            <div className="card-name">{partner.name}</div>
-          </div>
+          <Card key={index} className="card cardd">
+            <Card.Img
+              variant="top"
+              src={partner.image}
+              alt={partner.name}
+              className="card-img-top"
+            />
+            <Card.Body>
+              <Card.Title className="card-title text-center mt-3 mb-3 text-white">{partner.name}</Card.Title>
+            </Card.Body>
+          </Card>
         ))}
       </div>
-      <div className="view-all-container">
-        <button className="view-all-button" onClick={() => setShowAll(!showAll)}>
-          {showAll ? 'View Less' : 'View All'} <FaAngleDoubleDown />
-        </button>
-      </div>
+      {!showAll && (
+        <div className="view-all-container">
+          <button
+            className="view-all-button"
+            onClick={() => setShowAll(!showAll)}
+          >
+            View All <FaAngleDoubleDown />
+          </button>
+        </div>
+      )}
     </div>
   );
 };
