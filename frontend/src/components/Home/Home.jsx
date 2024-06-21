c4ace232c1dbc8054e4536b47267fe5850e242e3
Rithik Raja S <rithikraja28.rr@gmail.com>
2024-06-21 13:54:49 +0530

    home update

--- frontend/src/components/Home/Home.jsx
+++ frontend/src/components/Home/Home.jsx
@@ -1,68 +1,53 @@
-import React from 'react';
-import './Home.css';
+import React from "react";
+import { Carousel } from "react-bootstrap";
+import "./Home.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const slides = [
   {
-    title: 'Data Science Cg IOT',
-    subtitle: 'Stay Updated and Remain Competitive',
-    description: 'Unlock your potentialities and excel in the ever-expanding Data Science and IOT field.',
-    image: '/photo1.jpg',
+    title: "Data Science Cg IOT",
+    subtitle: "Stay Updated and Remain Competitive",
+    description:
+      "Unlock your potentialities and excel in the ever-expanding Data Science and IOT field.",
+    image: "/photo1.jpg",
   },
   {
-    title: 'AI and Machine Learning',
-    subtitle: 'Creating best versions of you',
-    description: 'Be at the top of your professional game in the booming field of Artificial Intelligence and Machine Learning.',
-    image: '/photo2.jpg',
+    title: "AI and Machine Learning",
+    subtitle: "Creating best versions of you",
+    description:
+      "Be at the top of your professional game in the booming field of Artificial Intelligence and Machine Learning.",
+    image: "/photo2.jpg",
   },
   {
-    title: 'Global Knowledge Technologies',
-    subtitle: 'Offering IT Courses in Disruptive Technologies',
-    description: 'Why settle for average when you deserve an elite career? Get versed in latest technologies including Blockchain, Cloud, Tableau and Data Science with us.',
-    image: '/photo3.jpg',
-  }
+    title: "Global Knowledge Technologies",
+    subtitle: "Offering IT Courses in Disruptive Technologies",
+    description:
+      "Why settle for average when you deserve an elite career? Get versed in latest technologies including Blockchain, Cloud, Tableau and Data Science with us.",
+    image: "/photo3.jpg",
+  },
 ];
 
-class Home extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      currentSlide: 0,
-    };
-  }
-
-  componentDidMount() {
-    this.slideInterval = setInterval(this.nextSlide, 3000);
-  }
-
-  componentWillUnmount() {
-    clearInterval(this.slideInterval);
-  }
-
-  nextSlide = () => {
-    this.setState((prevState) => ({
-      currentSlide: (prevState.currentSlide + 1) % slides.length,
-    }));
-  };
-
-  render() {
-    const { currentSlide } = this.state;
-    const { title, subtitle, description, image } = slides[currentSlide];
-
-    return (
-      <div className="home">
-        <div className="slide">
-          <div className="content">
-            <h3>{title}</h3>
-            <h4>{subtitle}</h4>
-            <p>{description}</p>
-          </div>
-          <div className="image-container">
-            <img src={image} alt={title} />
-          </div>
-        </div>
-      </div>
-    );
-  }
-}
+const Home = () => {
+  return (
+    <div className="home">
+      <Carousel fade interval={5000} controls={true} indicators={true}>
+        {slides.map((slide, index) => (
+          <Carousel.Item key={index}>
+            <div className="slide">
+              <div className="content">
+                <h3>{slide.title}</h3>
+                <h4>{slide.subtitle}</h4>
+                <p>{slide.description}</p>
+              </div>
+              <div className="image-container">
+                <img src={slide.image} alt={slide.title} />
+              </div>
+            </div>
+          </Carousel.Item>
+        ))}
+      </Carousel>
+    </div>
+  );
+};
 
 export default Home;
