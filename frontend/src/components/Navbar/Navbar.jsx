9f6bd31acd184d21eb62e47c1116f24748c23a49
Rithik Raja S <rithikraja28.rr@gmail.com>
2024-06-21 13:35:13 +0530

    navbar ui update

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,84 +1,103 @@
-import React from 'react';
-import './Navbar.css';
+import React from "react";
+import "./Navbar.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
 const Navbar = () => {
   return (
     <div>
       <section className="position-relative">
-        <nav className="navbar navbar-mainbg navbar-expand-lg navbar-dark bg-dark">
+        <nav className="navbar navbar-expand-lg">
           <div className="container">
             <a className="navbar-brand" href="#">
-              <img src="pstls-assets/logos/logo-purple-white.svg" alt="Logo" />
+              <img src="" alt="Logo" />
             </a>
-            <button className="btn p-0 d-lg-none navbar-burger" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent">
-              <svg
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  fillRule="evenodd"
-                  clipRule="evenodd"
-                  d="M3.5 7C3.22386 7 3 6.77614 3 6.5C3 6.22386 3.22386 6 3.5 6H20.5C20.7761 6 21 6.22386 21 6.5C21 6.7761 20.7761 7 20.5 7H3.5ZM3.5 12C3.22386 12 3 11.7761 3 11.5C3 11.2239 3.22386 11 3.5 11H20.5C20.7761 11 21 11.2239 21 11.5C21 11.7761 20.7761 12 20.5 12H3.5ZM3 16.5C3 16.7761 3.22386 17 3.5 17H20.5C20.7761 17 21 16.7761 21 16.5C21 16.2239 20.7761 16 20.5 16H3.5C3.22386 16 3 16.2239 3 16.5Z"
-                  fill="#fff"
-                ></path>
-              </svg>
+            <button
+              className="navbar-toggler border-0" 
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarSupportedContent"
+            >
+              <span className="navbar-toggler-icon"></span>
             </button>
-            <div className="collapse navbar-collapse" id="navbarSupportedContent">
-              <ul className="navbar-nav ms-1 mb-2 mb-lg-0">
+            <div
+              className="collapse navbar-collapse"
+              id="navbarSupportedContent"
+            >
+              <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                 <li className="nav-item">
-                  <a className="nav-link text-white" aria-current="page" href="#">
-                    ABOUT
+                  <a
+                    className="nav-link text-white"
+                    aria-current="page"
+                    href="#"
+                  >
+                    About us
+                  </a>
+                </li>
+
+                <li className="nav-item">
+                  <a className="nav-link text-white" href="#">
+                    Blog
                   </a>
                 </li>
                 <li className="nav-item">
                   <a className="nav-link text-white" href="#">
-                    OFFERS
+                    Contact us
                   </a>
                 </li>
-                <li className="nav-item">
-                  <a className="nav-link text-white" href="#">
-                    SCHEDULES
+                <li className="nav-item dropdown">
+                  <a
+                    className="nav-link dropdown-toggle text-white"
+                    href="#"
+                    id="collectionsDropdown"
+                    role="button"
+                    data-bs-toggle="dropdown"
+                    aria-expanded="false"
+                  >
+                    Collections
                   </a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link text-white" href="#">
-                    BLOG
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link text-white" href="#">
-                    WEBINARS
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link text-white" href="#">
-                    ACADEMICS
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link text-white" href="#">
-                    DIGITAL
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link text-white" href="#">
-                    AWARDS & RECOGNITION
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link text-white" href="#">
-                    CONTACT
-                  </a>
+                  <ul
+                    className="dropdown-menu dropdown-menu-dark"
+                    aria-labelledby="collectionsDropdown"
+                  >
+                    <li>
+                      <a className="dropdown-item" href="#">
+                        Offers
+                      </a>
+                    </li>
+                    <li>
+                      <a className="dropdown-item" href="#">
+                        Schedules
+                      </a>
+                    </li>
+                    <li>
+                      <a className="dropdown-item" href="#">
+                        Webinar
+                      </a>
+                    </li>
+                    <li>
+                      <a className="dropdown-item" href="#">
+                        Academics
+                      </a>
+                    </li>
+                    <li>
+                      <a className="dropdown-item" href="#">
+                        Digital
+                      </a>
+                    </li>
+                    <li>
+                      <a className="dropdown-item" href="#">
+                        Awards & Recognition
+                      </a>
+                    </li>
+                  </ul>
                 </li>
               </ul>
-              <div className="ms-auto">
-                <a className="btn text-white" href="#">
+              <div className="d-flex">
+                <a className="btn btn-outline-light me-2" href="#">
                   Log In
                 </a>
-                <a className="btn btn-light mt-3 mt-md-0" href="#">
+                <a className="btn btn-light" href="#">
                   Sign Up
                 </a>
               </div>
