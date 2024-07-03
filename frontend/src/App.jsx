f7cc9897b08f2162f4099bf4d056ffbf4b9e954d
Rithik Raja S <rithikraja28.rr@gmail.com>
2024-07-03 15:06:32 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,4 @@
-import React from 'react';
-import { Route, Routes } from 'react-router-dom';
-import LandingPage from './components/Landingpage/LandingPage';
-import Navbar from './components/Navbar/Navbar';
-import AboutPage from './pages/AboutPage/AboutPage';
-import Coursework from './components/Coursework/Coursework';
-import CourseDetail from './components/Coursework/CourseDetail'; // Import the new component
-import Footer from './components/Footer/Footer';
-import './components/Navbar/Navbar.module.css';
+
 // App.jsx
 
 import React from "react";
@@ -25,13 +17,6 @@ import GlobalStyle from "./GlobalStyle";
 function App() {
   return (
     <>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/aboutgkt" element={<AboutPage />} />
-        <Route path="/academicsgkt" element={<Coursework />} />
-        <Route path="/course/:courseCode" element={<CourseDetail />} /> 
-      </Routes>
       <GlobalStyle />
       <Navbar />
       <Container
