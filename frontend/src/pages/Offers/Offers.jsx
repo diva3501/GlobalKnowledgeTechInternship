93aebd39372587c53277c78b0679ebbcb7ccc970
hariniP-2503 <158076146+hariniP-2503@users.noreply.github.com>
2024-07-07 19:49:10 +0530

    offers

--- frontend/src/pages/Offers/Offers.jsx
+++ frontend/src/pages/Offers/Offers.jsx
@@ -17,9 +17,9 @@ function Offers() {
 
   const courses = [
     { id: 1, name: 'Red Hat Linux Administration', category: 5, description: 'Learn the essentials of Red Hat Linux administration.', image: 'https://kineo.com/glide/containers/main/case_studies_images/Redhat/microsoftteams-image-%2818%29-%281%29.png/27610923d84dab46f32f3d30bcbb1e53.png' },
-    { id: 2, name: 'Tableau Data Visualization', category: 6, description: 'Master data visualization techniques using Tableau.', image: 'https://img.freepik.com/free-vector/entrepreneur-analyzing-business-data-diagrams_1262-21466.jpg?t=st=1720205640~exp=1720209240~hmac=882cb6fed5787d4690340338295caad41fa180260fa71364bb5eda16ade0059e&w=900' },
-    { id: 3, name: 'Tableau Advanced', category: 6, description: 'Advance your skills in Tableau with advanced features.', image: 'https://img.freepik.com/free-vector/flat-hand-drawn-people-analyzing-growth-chart-illustration_23-2148859388.jpg?t=st=1720205712~exp=1720209312~hmac=25a8f22f31aea9afdb14f3e2cc95020e433120a8d5ea44021bd2e412a092b370&w=900' },
-    { id: 4, name: 'Tableau Expert', category: 6, description: 'Become a Tableau expert with in-depth knowledge.', image: 'https://img.freepik.com/premium-vector/market-forecast-trend-use-chart-graphic-vector-business-market-forecast-graph-data-financial-analysis-trend-illustration_102902-6006.jpg?w=900' },
+    { id: 2, name: 'Tableau Data Visualization ', category: 6, description: 'Master data visualization techniques using Tableau.', image: 'https://img.freepik.com/free-vector/entrepreneur-analyzing-business-data-diagrams_1262-21466.jpg?t=st=1720205640~exp=1720209240~hmac=882cb6fed5787d4690340338295caad41fa180260fa71364bb5eda16ade0059e&w=900' },
+    { id: 3, name: 'Tableau Intermediate', category: 6, description: 'Advance your skills in Tableau with advanced features.', image: 'https://img.freepik.com/free-vector/flat-hand-drawn-people-analyzing-growth-chart-illustration_23-2148859388.jpg?t=st=1720205712~exp=1720209312~hmac=25a8f22f31aea9afdb14f3e2cc95020e433120a8d5ea44021bd2e412a092b370&w=900' },
+    { id: 4, name: 'Tableau Advanced', category: 6, description: 'Become a Tableau expert with in-depth knowledge.', image: 'https://img.freepik.com/premium-vector/market-forecast-trend-use-chart-graphic-vector-business-market-forecast-graph-data-financial-analysis-trend-illustration_102902-6006.jpg?w=900' },
   ];
 
   // Calculate expected counts based on provided numbers
